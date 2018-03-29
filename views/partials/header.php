cc26afde54a7a76c7c1cd473262418689377419d
oliviernchima <nchima2010@gmail>
2018-03-29 16:26:53 +0200

    menu and footer

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,19 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+   <!-- jQuery library -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+   <!-- Popper JS -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+   <!-- Latest compiled JavaScript -->
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="assets/style.css">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Ubuntu" rel="stylesheet">
-    
+
+    <!-- font awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
     <title>Hackoustic</title>
 </head>
 <body>
-    
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light menu">
+    <a class="navbar-brand" href="index.php"> NOISE-AREA  </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav">
+    <a class="nav-item nav-link active" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+    <a class="nav-item nav-link" href="mesure.php">Mesure</a>
+    <a class="nav-item nav-link" href="map.php">Map</a>
+    <a class="nav-item nav-link disabled" href="sensibilisation.php">Sensibilisation</a>
+    <a class="nav-item nav-link disabled" href="parametres.php">Parametres</a>
+    <a class="nav-item nav-link disabled" href="contact.php">Contact</a>
+  </div>
+</div>
+</nav>
+  </div>
