ecda6d2e843c253dafa6264c22f61d2bd57e4d35
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-03-29 17:06:40 +0200

    google map callback order

--- views/pages/map.php
+++ views/pages/map.php
@@ -3,24 +3,6 @@
 <div id="map" style="height:550px;width:50%;"></div>
 
 <script type="text/javascript">
-  $.ajax({
-    url:'https://api.irisnetlab.be:443/api/happyears/0.1.0/places',
-    headers:{
-      Authorization:'Bearer 1b67da63-e6d8-3ad9-aed0-cc7d1cd91618',
-      Accept:"application/json"
-    }
-  }).done(function (data) {
-    console.log(data);
-    for (coord of data.features) {
-      console.log(coord.geometry.coordinates);
-      console.log({lat: coord.geometry.coordinates[1], lng: coord.geometry.coordinates[0]});
-      var marker = new google.maps.Marker({
-        position: {lat: parseFloat(coord.geometry.coordinates[1]), lng: parseFloat(coord.geometry.coordinates[0])},
-        map: window.map
-      });
-    }
-  });
-
   function initMap() {
     var center = {lat: 50.8455124, lng: 4.3552839};
      window.map = new google.maps.Map(document.getElementById('map'), {
@@ -31,6 +13,25 @@
       position: center,
       map: map
     });
+    
+    $.ajax({
+      url:'https://api.irisnetlab.be:443/api/happyears/0.1.0/places',
+      headers:{
+        Authorization:'Bearer 1b67da63-e6d8-3ad9-aed0-cc7d1cd91618',
+        Accept:"application/json"
+      }
+    }).done(function (data) {
+      console.log(data);
+      for (coord of data.features) {
+        console.log(coord.geometry.coordinates);
+        console.log({lat: coord.geometry.coordinates[1], lng: coord.geometry.coordinates[0]});
+        var marker = new google.maps.Marker({
+          position: {lat: parseFloat(coord.geometry.coordinates[1]), lng: parseFloat(coord.geometry.coordinates[0])},
+          map: window.map
+        });
+      }
+    });
+
   }
 
 </script>
