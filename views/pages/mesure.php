284118ad827a878d288f2a8fa7309a9bd74c6d39
Aron Dadi <aron.d.dadi@gmail.com>
2018-03-30 09:19:38 +0200

    page mesure

---
+++ views/pages/mesure.php
@@ -0,0 +1,13 @@
+<div class="content">
+    <h2>Bienvenue sur la page de visualisation du son</h2>
+</div>
+
+<div id="sound">
+</div>
+
+<div class="content">
+    <input type="submit" name="sendSound"value="Send the sound!">
+    <p>Vert: Tout est bon !</p>
+    <p>Orange:Moyen moyen tout ça !</p>
+    <p>Rouge: Part d'ici !</p>
+</div>
