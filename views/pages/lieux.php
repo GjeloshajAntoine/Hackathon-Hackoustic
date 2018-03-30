766b4be6be77cc2d0fb1b3375fb57fd08d87c8f1
oliviernchima <nchima2010@gmail>
2018-03-30 11:47:19 +0200

    fin de la page lieux

--- views/pages/lieux.php
+++ views/pages/lieux.php
@@ -1,114 +1,135 @@
 
-<h4 class="text-center lieux"> Informations sur les lieux </h4>
+<h4 class="text-center lieux"> Informations sonore </h4>
 <table class="table table-striped">
   <thead>
     <tr>
       <th scope="col">Debut</th>
       <th scope="col">Fin</th>
-      <th scope="col">NIveau Sonore</th>
+      <th scope="col">Niveau Sonore</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td scope="row">00:00:00</td>
-      <td>01.00.00</td>
-      <td>Calme</td>
+      <td scope="row">00:00</td>
+      <td>01:00</td>
+      <td class="calme">Calme</td>
     </tr>
     <tr>
-      <td scope="row">01.00.00</td>
-      <td>02.00.00</td>
-      <td>Calme</td>
+      <td scope="row">01:00</td>
+      <td>02:00</td>
+      <td class="calme">Calme</td>
     </tr>
     <tr>
-      <td scope="row">02.00.00</td>
-      <td>03.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">02:00</td>
+      <td>03:00</td>
+      <td class="calme">Calme</td>
     </tr>
     <tr>
-      <td scope="row">03.00.00</td>
-      <td>04.00.00</td>
-      <td>calme</td>
+      <td scope="row">03:00</td>
+      <td>04:00</td>
+      <td class="calme">Calme</td>
     </tr>
     <tr>
-      <td scope="row">04.00.00</td>
-      <td>05.00.00</td>
-      <td>calme</td>
+      <td scope="row">04:00</td>
+      <td>05:00</td>
+      <td class="calme">Calme</td>
     </tr>
     <tr>
-      <td scope="row">05.00.00</td>
-      <td>06.00.00</td>
-      <td>calme</td>
+      <td scope="row">05:00</td>
+      <td>06:00</td>
+      <td class="calme">Calme</td>
     </tr>
     <tr>
-      <td scope="row">06.00.00</td>
-      <td>07.00.00</td>
-      <td>calme</td>
+      <td scope="row">06:00</td>
+      <td>07:00</td>
+      <td class="calme">Calme</td>
     </tr>
     <tr>
-      <td scope="row">07.00.00</td>
-      <td>08.00.00</td>
-      <td>Moins-calme</td>
+      <td scope="row">07:00</td>
+      <td>08:00</td>
+      <td class="normal">Normal</td>
     </tr>
     <tr>
-      <td scope="row">08.00.00</td>
-      <td>09.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">08:00</td>
+      <td>09:00</td>
+      <td class="normal">Normal</td>
     </tr>
     <tr>
-      <td scope="row">09.00.00</td>
-      <td>10.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">09:00</td>
+      <td>10:00</td>
+      <td class="normal">Normal</td>
     </tr>
     <tr>
-      <td scope="row">10.00.00</td>
-      <td>11.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">10:00</td>
+      <td> 11:00 </td>
+      <td class="calme"> Calme </td>
     </tr>
     <tr>
-      <td scope="row">11.00.00</td>
-      <td>12.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">11:00</td>
+      <td>12:00</td>
+      <td class="calme"> Calme</td>
     </tr>
     <tr>
-      <td scope="row">12.00.00</td>
-      <td>13.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">12:00</td>
+      <td>13:00</td>
+      <td class="nuisible"> Nuisible </td>
     </tr>
     <tr>
-      <td scope="row">13.00.00</td>
-      <td>14.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">13:00</td>
+      <td>14:00</td>
+      <td class="normal"> Normal</td>
     </tr>
     <tr>
-      <td scope="row">14.00.00</td>
-      <td>15.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">14:00</td>
+      <td>15:00</td>
+      <td class="normal">Normal</td>
     </tr>
     <tr>
-      <td scope="row">15.00.00</td>
-      <td>16.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">15:00</td>
+      <td>16:00</td>
+      <td class="normal"> Normal </td>
     </tr>
     <tr>
-      <td scope="row">16.00.00</td>
-      <td>17.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">16:00</td>
+      <td>17:00</td>
+      <td class="normal"> Normal </td>
     </tr>
     <tr>
-      <td scope="row">17.00.00</td>
-      <td>18.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">17:00</td>
+      <td>18:00</td>
+      <td class="normal"> Normal </td>
     </tr>
     <tr>
-      <td scope="row">19.00.00</td>
-      <td>20.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row">17:00</td>
+      <td>18:00</td>
+      <td class="nuisible"> Nuisible </td>
     </tr>
     <tr>
-      <td scope="row">20.00.00</td>
-      <td>21.00.00</td>
-      <td>Tres calme</td>
+      <td scope="row normal">19:00</td>
+      <td>20:00</td>
+      <td class="normal"> Normal</td>
     </tr>
+    <tr>
+      <td scope="row">20:00</td>
+      <td>21:00</td>
+      <td class="calme">Normal</td>
+    </tr>
+    <tr>
+      <td scope="row">21:00</td>
+      <td>22:00</td>
+      <td class="calme"> Calme</td>
+    </tr>
+    <tr>
+      <td scope="row calme">22:00</td>
+      <td>23:00</td>
+      <td class="calme"> Calme </td>
+    </tr>
+    <tr>
+      <td scope="row">23:00</td>
+      <td>23:59</td>
+      <td class="calme"> Calme </td>
+    </tr>
+
   </tbody>
 </table>
 
