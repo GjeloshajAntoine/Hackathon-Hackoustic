935b4e07800b1d24dbb842cf3c98abc219896c80
Ja-R <jacqueline_rwa@msn.com>
2018-03-30 14:06:06 +0200

    modif

--- views/pages/mesure.php
+++ views/pages/mesure.php
@@ -7,9 +7,9 @@
     <p><span id="borne" style="display:none;">Borne detectée , Bienvenue chez <strong>Becode</strong> </span> </p>
     <br>
     <br>
-    <p><span class="textGreen">Vert:</span> Tout est bon ! En dessous de 75decibels.</p>
-    <p><span class="textOrange">Orange:</span> Moyen moyen tout ça ! Entre 75 et 95decibels.</p>
-    <p><span class="textRed">Rouge:</span> Partez d'ici ! Au dessus de  95decibels.</p>
+    <p><span class="textGreen">Vert:</span> Tout est bon ! En dessous de 75 décibels.</p>
+    <p><span class="textOrange">Orange:</span> Moyen moyen tout ça ! Entre 75 et 95décibels.</p>
+    <p><span class="textRed">Rouge:</span> Partez d'ici ! Au-dessus de 95 décibels.</p>
     <br>
     <button id="sendData" class="btn-success">Envoyez</button>
 
